3de47d077b5d950532b2e952116fda6067210d61
Muhammed Javis <muhammedjavis@gmail.com>
2023-11-30 01:29:12 +0530

    Added mobile responsive view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-
+import { useMediaQuery } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Link from '@mui/material/Link';
 import { NavLink, useLocation } from 'react-router-dom';
+import { useState } from 'react';
 
 function Header({ sections }) {
   const activeColor = '#ADD8E6';
   const location = useLocation();
+  const isMobile = useMediaQuery('(max-width:600px)');
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <>
@@ -19,44 +32,73 @@ function Header({ sections }) {
           justifyContent: 'space-between',
         }}
       >
-        <Button size='small'>High Contrast</Button>
-
-        <Button variant='contained' size='small'>
+        <Button size='small'>High/Low Contrast</Button>
+        <Button variant='contained' size='small' onClick={handleClick}>
           <NavLink
             to='/login'
-            style={{ textDecoration: 'none', color: 'white' }}
+            style={{ textDecoration: 'none', color: 'inherit' }}
           >
             Login
           </NavLink>
         </Button>
       </Toolbar>
       <Toolbar component='nav' variant='dense'>
-        {sections.map((section) => {
-          const isActive = location.pathname === section.url;
-
-          return (
-            <Link
-              color='inherit'
-              key={section.title}
-              variant='body2'
-              href={section.url}
-              underline='none'
-              sx={{
-                p: 1,
-                flexShrink: 0,
-                textDecoration: 'none',
-                fontWeight: 'bold',
-                color: isActive ? activeColor : 'inherit',
-                whiteSpace: 'nowrap',
-                '&:hover': {
-                  color: activeColor,
-                },
-              }}
+        {isMobile ? (
+          <div>
+            <Button
+              aria-controls='nav-menu'
+              aria-haspopup='true'
+              onClick={handleClick}
+              size='small'
             >
-              {section.title}
-            </Link>
-          );
-        })}
+              Pages
+            </Button>
+            <Menu
+              id='nav-menu'
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {sections.map((section) => (
+                <NavLink
+                  key={section.title}
+                  to={section.url}
+                  style={{ textDecoration: 'none', color: 'inherit' }}
+                  onClick={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>{section.title}</MenuItem>
+                </NavLink>
+              ))}
+            </Menu>
+          </div>
+        ) : (
+          sections.map((section) => {
+            const isActive = location.pathname === section.url;
+            return (
+              <Link
+                color='inherit'
+                key={section.title}
+                variant='body2'
+                href={section.url}
+                underline='none'
+                sx={{
+                  p: 1,
+                  flexShrink: 0,
+                  textDecoration: 'none',
+                  fontWeight: 'bold',
+                  color: isActive ? activeColor : 'inherit',
+                  whiteSpace: 'nowrap',
+                  '&:hover': {
+                    color: activeColor,
+                  },
+                }}
+              >
+                {section.title}
+              </Link>
+            );
+          })
+        )}
       </Toolbar>
     </>
   );
